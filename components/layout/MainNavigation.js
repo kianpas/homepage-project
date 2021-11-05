893971601c461b11529e0cc349b5d6cc18ca7b5f
kianpas <kianpas@gmail.com>
2021-11-05 19:40:28 +0900

    2021.11.05 반응형 사이드바 처리

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -1,9 +1,22 @@
 import Link from "next/link";
+import { useState } from "react";
 import classes from "./MainNavigation.module.css";
 
 const MainNavigation = (props) => {
+  const [sideMenu, setSideMenu] = useState(false);
+  const menuHandler = () => {
+    setSideMenu(true);
+    if (sideMenu) {
+      setSideMenu(false);
+    }
+  };
+
   return (
-    <header className={classes.header}>
+    <header
+      className={
+        sideMenu ? `${classes.header} ${classes.on}` : `${classes.header}`
+      }
+    >
       <div className={classes.logo}>Reindeer</div>
       <nav>
         <ul>
@@ -26,6 +39,7 @@ const MainNavigation = (props) => {
             <Link href="/contact">CONTACT</Link>
           </li>
         </ul>
+        <i className="bx bx-menu" onClick={menuHandler}></i>
       </nav>
     </header>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,10 @@ import "../styles/globals.css";
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <link
+          href="https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css"
+          rel="stylesheet"
+        ></link>
       <Component {...pageProps} />
     </Layout>
   );
