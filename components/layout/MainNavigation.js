5ac69e0ce7cf26462767740f5fe67bb69e4c8ee3
kianpas <kianpas@gmail.com>
2021-11-08 22:46:45 +0900

    2021.11.08 사이드메뉴, 백드롭 처리

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import classes from "./MainNavigation.module.css";
 
 const MainNavigation = (props) => {
@@ -13,38 +13,46 @@ const MainNavigation = (props) => {
     }
   };
 
+  const backDropHandler = () => {
+    setSideMenu(false);
+  };
+
   return (
-    <header
-      className={
-        sideMenu ? `${classes.header} ${classes.toggle}` : `${classes.header}`
-      }
-    >
-      {sideMenu && <div className={classes.backdrop}></div>}
-      <div className={classes.logo}>Reindeer</div>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/">HOME</Link>
-          </li>
-          <li>
-            <Link href="/about">ABOUT</Link>
-          </li>
-          <li>
-            <Link href="/project">PROJECT</Link>
-          </li>
-          <li>
-            <Link href="/roadmap">ROAD MAP</Link>
-          </li>
-          <li>
-            <Link href="/hobby">HOBBY</Link>
-          </li>
-          <li>
-            <Link href="/contact">CONTACT</Link>
-          </li>
-        </ul>
-      </nav>
-      <i className="bx bx-menu" onClick={menuHandler}></i>
-    </header>
+    <Fragment>
+      <header
+        className={
+          sideMenu ? `${classes.header} ${classes.toggle}` : `${classes.header}`
+        }
+      >
+        <div className={classes.logo}>Reindeer</div>
+        <nav>
+          <ul>
+            <li>
+              <Link href="/">HOME</Link>
+            </li>
+            <li>
+              <Link href="/about">ABOUT</Link>
+            </li>
+            <li>
+              <Link href="/project">PROJECT</Link>
+            </li>
+            <li>
+              <Link href="/roadmap">ROAD MAP</Link>
+            </li>
+            <li>
+              <Link href="/hobby">HOBBY</Link>
+            </li>
+            <li>
+              <Link href="/contact">CONTACT</Link>
+            </li>
+          </ul>
+        </nav>
+        {!sideMenu && <i className="bx bx-menu" onClick={menuHandler}></i>}
+      </header>
+      {sideMenu && (
+        <div className={classes.backdrop} onClick={backDropHandler}></div>
+      )}
+    </Fragment>
   );
 };
 
